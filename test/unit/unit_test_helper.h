799b1a6d8c73d0e80769f0b84516ae4cb6227209
Walter Van Herck <Walter.Vanherck@iter.org>
2024-07-02 11:23:15 +0000

    Adapt to rename Setup -> Teardown

--- test/unit/unit_test_helper.h
+++ test/unit/unit_test_helper.h
@@ -68,7 +68,7 @@ static inline bool TryAndExecute(std::unique_ptr<Procedure>& proc,
   bool status = TryAndExecuteNoReset(proc, ui, expect);
   if (proc)
   {
-    proc->Reset(ui);
+    proc->Teardown(ui);
   }
   return status;
 }
