314920609e06b036dfb9950cea7ee3ae338b1c46
Walter Van Herck <Walter.Vanherck@iter.org>
2024-11-22 08:33:01 +0000

    Fix after making Procedure::Teardown private

--- test/unit/unit_test_helper.h
+++ test/unit/unit_test_helper.h
@@ -68,7 +68,7 @@ static inline bool TryAndExecute(std::unique_ptr<Procedure>& proc,
   bool status = TryAndExecuteNoReset(proc, ui, expect);
   if (proc)
   {
-    proc->Teardown(ui);
+    proc->Reset(ui);
   }
   return status;
 }
