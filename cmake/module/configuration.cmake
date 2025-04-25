65bde9079ff04ce18f2fe405dc400b1e0379f024
Walter Van Herck <walter.vanherck@iter.org>
2025-04-25 13:14:49 +0000

    Use common COA cmake config file

--- cmake/module/configuration.cmake
+++ cmake/module/configuration.cmake
@@ -1,34 +1,16 @@
 # -----------------------------------------------------------------------------
-# Main project configuration
+# Common project configuration
 # -----------------------------------------------------------------------------
 
-include(GNUInstallDirs)
-include(CTest)
+include(COACommonConfig)
 include(COASetupCodacEnvironment)
 include(FetchGithubDeps)
-
-# -----------------------------------------------------------------------------
-# C++ version
-# -----------------------------------------------------------------------------
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
-if(NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE "RelWithDebInfo")
-endif()
-
-# -----------------------------------------------------------------------------
-# Variables
-# -----------------------------------------------------------------------------
-set(LIBVERSION ${CMAKE_PROJECT_VERSION})
-set(LIBSOVERSION ${CMAKE_PROJECT_VERSION_MAJOR}.${CMAKE_PROJECT_VERSION_MINOR})
 
 # -----------------------------------------------------------------------------
 # Directories
 # -----------------------------------------------------------------------------
 if (NOT DEFINED PLUGIN_PATH)
-  set(PLUGIN_PATH lib/oac-tree/plugins)
+  set(PLUGIN_PATH ${CMAKE_INSTALL_LIBDIR}/oac-tree/plugins)
 endif()
 message(DEBUG "PLUGIN_PATH: ${PLUGIN_PATH}")
 
@@ -36,12 +18,7 @@ if (NOT DEFINED PLUGIN_RUNTIME_DIRECTORY)
   set(PLUGIN_RUNTIME_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${PLUGIN_PATH})
 endif()
 
-if (NOT DEFINED TEST_OUTPUT_DIRECTORY)
-  set(TEST_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/test_bin)
-endif()
-
 file(MAKE_DIRECTORY ${PLUGIN_RUNTIME_DIRECTORY})
-file(MAKE_DIRECTORY ${TEST_OUTPUT_DIRECTORY})
 
 # -----------------------------------------------------------------------------
 # Dependencies
